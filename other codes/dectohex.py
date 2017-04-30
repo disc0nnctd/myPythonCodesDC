104ee3bc4511c8178120513220b5252fc75d16e6
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-04-30 15:56:51 +0530

    Decimal to Hex converter
    Update: Added PEP styling

--- other codes/dectohex.py
+++ other codes/dectohex.py
@@ -1,9 +1,13 @@
-hexa={1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9',10:'A',11:'B',12:'C',13:'D', 14:'E',15:'F'}
-a=input("enter decimal")
-q=a
-s=''
-while q>0:
-    r=q%16
-    q=q/16
-    s+=hexa[r]
-    f=s[::-1]
+"""Converts decimal to hexadecimal using dictionaries."""
+
+
+hexa = {1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+        10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+a = input("enter decimal")
+q = a
+s = ''
+while q > 0:
+    r = q % 16
+    q = q/16
+    s += hexa[r]
+    f = s[::-1]
