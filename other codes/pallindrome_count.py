02c363b94ca1fd28445c0f4753a26a8141c7defb
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-04-30 16:04:56 +0530

    Pallindrome Counter
    Update:
    -Shows which terms are pallindromes.
    -PEP styling added

--- other codes/pallindrome_count.py
+++ other codes/pallindrome_count.py
@@ -1,13 +1,18 @@
-#count pallindromes in a sentence
+"""Counts pallindromes in a sentence."""
+
+
 def pal(word):
     return word == word[::-1]
 
-s= raw_input("Enter a sentence: ").split()
 
+s = raw_input("Enter a sentence: ").split()
 c = 0
+l = []
 for w in s:
     if pal(w):
-        c+= 1
+        c += 1
+        l.append(w)
     else:
         break
-print " There are %s palindromes in your sentence\n."%(c)
+print " There are %s palindromes in your sentence." % (c)
+print "The following list shows the pallindromes in your sentence: \n", l
