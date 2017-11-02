d79e11789827227776d3eda10d9ceb90ca2a236c
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-11-02 14:49:13 +0530

    Add files via upload

---
+++ Class12PracticalAssignment/q29pallindromes.py
@@ -0,0 +1,26 @@
+def writer():
+    a=open("Words.txt","w")
+    b=input("How many words to input? ")
+    for i in range(b):
+        c=raw_input("Word %s: "%(i+1))
+        a.write(c+"\n")
+    a.close()
+def pal(x):
+    if x[::-1]==x:
+        return True
+    else:
+        return False
+def list_pallindrome():
+    try:    
+        a=open("Words.txt","r")
+        b=a.readlines()
+        l=[]
+        for i in b:
+            l.append(i[:-1])
+        for j in l:
+            if pal(j):
+                print j
+    except EOFError:
+        pass
+writer()
+list_pallindrome()
