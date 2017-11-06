9a5de245ce99408a3dede60853b622f9a4b242c2
DivyanshDC <Divyanshchhablani00@gmail.com>
2017-11-06 13:20:02 +0530

    Fixed bad code

--- Class12PracticalAssignment/q1SwapEvenOdd.py
+++ Class12PracticalAssignment/q1SwapEvenOdd.py
@@ -2,19 +2,19 @@
 
 A function that takes a list and
 its size as arguements and swaps the elements of every even location
-with its following odd location
-"""
-# you'll have to call the function again to use it.
+with its following odd location.
 
+However, if the number of elements in the list is odd, the last element will be ignored.
+"""
 
 def swapeo(l, size):
     """"The function that swaps."""
-    for i in range(size-1):
-        pos = i+1
-        if pos % 2 == 0:
-            l[i], l[i+1] = l[i+1], l[i]
-    return l
-
+    if len(l)%2==0:
+        rnge=len(l)
+    else:
+        rnge=len(l)-1
+    for i in range(0,rnge,2):
+        l[i], l[i+1]=l[i+1], l[i]
 
 l = input("Enter List")
 size = len(l)
